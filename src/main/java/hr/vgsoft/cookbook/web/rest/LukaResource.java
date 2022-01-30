88832e0fb18f3e522505e43347618c526bcb40f1
LukaCukeric <lukacukeric77@gmail.com>
2022-01-30 22:26:35 +0100

    Refactoring

--- src/main/java/hr/vgsoft/cookbook/web/rest/LukaResource.java
+++ src/main/java/hr/vgsoft/cookbook/web/rest/LukaResource.java
@@ -3,13 +3,17 @@ package hr.vgsoft.cookbook.web.rest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/api/luka")
 public class LukaResource {
 
+    @Value("${cookbook.luka.test-string}")
+    private String testValue;
 
     @GetMapping("/test")
-    public String getTestString(@Value("${cookbook.luka.test-string}") String testValue) {
+    public String getTestString() {
         return testValue;
     }
 
