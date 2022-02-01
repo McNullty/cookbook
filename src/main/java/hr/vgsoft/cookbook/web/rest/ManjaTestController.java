f9bf764f31fb30d7ece46d8e28a24c1c252dbaf7
Manja Garaj <manja.garaj@ericsson.com>
2022-02-01 09:29:44 +0100

    updates for ManjaTestContorller

--- src/main/java/hr/vgsoft/cookbook/web/rest/ManjaTestController.java
+++ src/main/java/hr/vgsoft/cookbook/web/rest/ManjaTestController.java
@@ -1,21 +1,25 @@
 package hr.vgsoft.cookbook.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Controller
+@RestController
 @RequestMapping("/api/manja") 
 public class ManjaTestController {
 	
+	private final Logger log = LoggerFactory.getLogger(UserResource.class);
+	
 	@Value("${cookbook.manja.test-string}")
 	private String testString;
+	
 	@GetMapping("/test")
 	public String getString() {	
-		System.out.println(testString);
-		return testString;
-		
+		log.debug(testString);
+		return testString;		
 	}
 
 }
